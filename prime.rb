e57908eec286e21d47cbf3b9b9f4f59157663c99
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 17:56:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(number)
   
-  (2..number/2).any? do |number| 
+  (2..-1).each do |number| 
   if number <= 1 || number  == 0
     false
+  elsif number >= 2 
+    true
   end
-  true
+  
 end
 end
