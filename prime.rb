5317a983f5e522249698a16c1609e5d702d11242
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 17:49:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,9 @@
-def prime?(int)
- prime_int = []
-  if value <= 1 || value == 0 || value == 1
+def prime?(number)
+  
+  (2..number/2).none? do |n|
+    if number <= 1 || number == 0
       false
-    
-  elsif
-    (2..value - 1).each do |i|
-      prime_int <<
-      if value % i == 0
-      false
-      end
-  end
-  prime_int
-end
-end
+    end
 
+end
+end
